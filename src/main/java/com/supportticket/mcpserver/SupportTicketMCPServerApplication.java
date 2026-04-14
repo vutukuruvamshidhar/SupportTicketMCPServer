c537478c32071bc317c319a1522b64fdecd593f0
Vamshidhar Vutukuru <vutukuruvamshidhar@gmail.com>
2026-04-14 23:33:19 +0200

    Initial commit

---
+++ src/main/java/com/supportticket/mcpserver/SupportTicketMCPServerApplication.java
@@ -0,0 +1,13 @@
+package com.supportticket.mcpserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupportTicketMCPServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SupportTicketMCPServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/supportticket/mcpserver/SupportTicketMCPServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.supportticket.mcpserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupportTicketMCPServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
